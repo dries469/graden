fa8ecadf5ff0626e8f07372deba6e68ac28635b9
dries469 <124346362+dries469@users.noreply.github.com>
2023-03-07 12:22:58 +0100

    Initiële bestanden voor MakeCode-project

---
+++ main.ts
@@ -0,0 +1,43 @@
+input.onGesture(Gesture.LogoDown, function () {
+    basic.showArrow(ArrowNames.South)
+    basic.showLeds(`
+        . . # . .
+        . . # . .
+        # . # . #
+        . # # # .
+        . . # . .
+        `)
+})
+input.onGesture(Gesture.TiltRight, function () {
+    basic.showArrow(ArrowNames.West)
+    basic.showLeds(`
+        . . # . .
+        . # . . .
+        # # # # #
+        . # . . .
+        . . # . .
+        `)
+})
+input.onGesture(Gesture.LogoUp, function () {
+    basic.showArrow(ArrowNames.North)
+    basic.showLeds(`
+        . . # . .
+        . # # # .
+        # . # . #
+        . . # . .
+        . . # . .
+        `)
+})
+input.onGesture(Gesture.TiltLeft, function () {
+    basic.showArrow(ArrowNames.West)
+    basic.showLeds(`
+        . . # . .
+        . . . # .
+        # # # # #
+        . . . # .
+        . . # . .
+        `)
+})
+basic.forever(function () {
+	
+})
